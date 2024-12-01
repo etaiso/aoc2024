e9629de0693ec8bd41c154e2a95009bf4403a317
Etai Solomon <etais@wix.com>
2024-12-01 23:42:18 +0200

    improve times day 1

--- src/day_01.js
+++ src/day_01.js
@@ -3,16 +3,12 @@
  * Puzzle Description: https://adventofcode.com/2024/day/1
  */
 
-const getSortedLists = (lines) => {
-  const { left, right } = lines.reduce((acc, line) => {
-    const [l, r] = line.trim().split(/\s+/).map(Number);
-    acc.left.push(l);
-    acc.right.push(r);
-    return acc;
-  }, { left: [], right: [] });
-
-  return { sortedLeft: left.sort((a, b) => a - b), sortedRight: right.sort((a, b) => a - b) };
-}
+const getLists = (lines) => lines.reduce((acc, line) => {
+  const [l, r] = line.trim().split(/\s+/).map(Number);
+  acc.left.push(l);
+  acc.right.push(r);
+  return acc;
+}, { left: [], right: [] })
 
 /**
  * Returns the solution for level one of this puzzle.
@@ -22,7 +18,10 @@ const getSortedLists = (lines) => {
  * @returns {Number|String}
  */
 export const levelOne = ({ input, lines }) => {
-  const { sortedLeft, sortedRight } = getSortedLists(lines);
+  const { left, right } = getLists(lines);
+
+  const sortedLeft = left.sort((a, b) => a - b);
+  const sortedRight = right.sort((a, b) => a - b);
 
   return sortedLeft.reduce((acc, leftValue, index) => {
     return acc + Math.abs(leftValue - sortedRight[index]);
@@ -37,10 +36,14 @@ export const levelOne = ({ input, lines }) => {
  * @returns {Number|String}
  */
 export const levelTwo = ({ input, lines }) => {
-  const { sortedLeft, sortedRight } = getSortedLists(lines);
+  const { left, right } = getLists(lines);
 
-  return sortedLeft.reduce((acc, leftValue) => {
-    const rightOccurrences = sortedRight.filter((rightValue) => rightValue === leftValue).length;
-    return acc + leftValue * rightOccurrences;
+  const rightOccurrences = right.reduce((acc, rightValue) => {
+    acc[rightValue] = (acc[rightValue] || 0) + 1;
+    return acc;
+  }, {});
+
+  return left.reduce((acc, leftValue) => {
+    return acc + leftValue * (rightOccurrences[leftValue] || 0);
   }, 0);
 };
