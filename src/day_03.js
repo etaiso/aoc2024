2c73f8c9ff0177c5335a9894f7418da5d6e68f66
Etai Solomon <etais@wix.com>
2024-12-03 11:40:21 +0200

    day 3

--- src/day_03.js
+++ src/day_03.js
@@ -3,6 +3,37 @@
  * Puzzle Description: https://adventofcode.com/2024/day/3
  */
 
+const filterDonts = (input) => {
+  const parts = input.split(/(do\(\)|don't\(\))/);
+  let currentKey = "do";
+
+  return parts.reduce((acc, part) => {
+    if (part === "do()") {
+      currentKey = "do";
+    } else if (part === "don't()") {
+      currentKey = "dont";
+    } else {
+      if (currentKey === "do") {
+        return acc + part;
+      }
+    }
+    return acc;
+  }, "");
+};
+
+const getSumOfAllMuls = (input) => {
+  const regex = /mul\((\d{1,3}),(\d{1,3})\)/g;
+
+  const matches = Array.from(input.matchAll(regex), (match) => [
+    Number(match[1]),
+    Number(match[2]),
+  ]);
+
+  return matches.reduce((acc, [x, y]) => {
+    return acc + x * y;
+  }, 0);
+};
+
 /**
  * Returns the solution for level one of this puzzle.
  * @param {Object} args - Provides both raw and split input.
@@ -11,7 +42,7 @@
  * @returns {Number|String}
  */
 export const levelOne = ({ input, lines }) => {
-  // your code here
+  return getSumOfAllMuls(input);
 };
 
 /**
@@ -22,5 +53,5 @@ export const levelOne = ({ input, lines }) => {
  * @returns {Number|String}
  */
 export const levelTwo = ({ input, lines }) => {
-  // your code here
+  return getSumOfAllMuls(filterDonts(input));
 };
